f323edfb7dd729fed2d20e37955feb64a9a22363
Sanjay Santra <sanjaysantra@hotmail.com>
2019-02-22 12:48:11 +0530

    Update Form1.cs
    It can now handle HAR files.

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,11 @@ using System.Drawing;
 using System.Windows.Forms;
 using System.IO.Compression;
 using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 
 namespace RemoveFiddlerPassword
 {
@@ -18,76 +23,109 @@ namespace RemoveFiddlerPassword
         private void Form1_Load(object sender, EventArgs e)
         {                       
             richTextBox1.Visible = false;            
-            openFileDialog1.DefaultExt = ".saz"; // Default file extension
-            openFileDialog1.Filter = "Fiddler Trace Files (.saz)|*.saz"; // Filter files by extension
+            openFileDialog1.DefaultExt = ".saz,.har"; // Default file extension            
+            openFileDialog1.Filter = "Fiddler Trace Files |*.saz|HTTP Archives|*.har";
         }
 
-        public void ReadFile(string zipPath)
+        public void ReadFile(string zipPath, string pathExtension)
         {
             bool success = false;
-            string document = "";           
+            string document = "";          
 
             try
-            {              
-                using (var archive = ZipFile.Open(zipPath, ZipArchiveMode.Update))
+            {
+                if (pathExtension == ".saz")
                 {
-                    progBar.Minimum = 0;
-                    progBar.Maximum = archive.Entries.Count;                   
-                    progBar.Visible = true;
-
-                    int progressCount = 0;                    
-
-                    foreach (ZipArchiveEntry entry in archive.Entries)
+                    using (var archive = ZipFile.Open(zipPath, ZipArchiveMode.Update))
                     {
-                        progressCount++;
-                        progBar.Value = progressCount;
-                        progBar.Refresh();
-                        progBar.CreateGraphics().DrawString("Processing... " 
-                            + Math.Round((((double)progressCount/(double)progBar.Maximum)*100)).ToString() + "%", 
-                            new Font("Arial",
-                            (float)8.25, FontStyle.Regular),
-                            Brushes.Red, 
-                            new PointF(progBar.Width / 2 - 10, progBar.Height / 2 - 7));
+                        progBar.Minimum = 0;
+                        progBar.Maximum = archive.Entries.Count;
+                        progBar.Visible = true;
 
-                        if (entry.FullName.EndsWith(".txt", StringComparison.OrdinalIgnoreCase))
+                        int progressCount = 0;
+
+                        foreach (ZipArchiveEntry entry in archive.Entries)
                         {
-                            using (var stream = entry.Open())
-                            using (var reader = new StreamReader(stream))
-                            {
-                                document = reader.ReadToEnd();
-                                if (document.ToLower().Contains("passwd"))
-                                {
-                                    if (!document.ToLower().Contains("deleted"))
-                                    {
-                                        document = document.Replace(document.Substring((document.IndexOf("passwd") + 7), 
-                                            (document.IndexOf("&ps") - (document.IndexOf("passwd") + 7))), 
-                                            "DELETED"); 
-                                        stream.SetLength(text.Length);
+                            progressCount++;
+                            progBar.Value = progressCount;
+                            progBar.Refresh();
+                            progBar.CreateGraphics().DrawString("Processing... "
+                                + Math.Round((((double)progressCount / (double)progBar.Maximum) * 100)).ToString() + "%",
+                                new Font("Arial",
+                                (float)8.25, FontStyle.Regular),
+                                Brushes.Red,
+                                new PointF(progBar.Width / 2 - 10, progBar.Height / 2 - 7));
 
-                                        using (StreamWriter writer = new StreamWriter(stream))
-                                        {
-                                            writer.Write(document);
-                                            success = true;
-                                        }
-                                    }
-                                    else
+                            if (entry.FullName.EndsWith(".txt", StringComparison.OrdinalIgnoreCase))
+                            {
+                                using (var stream = entry.Open())
+                                using (var reader = new StreamReader(stream))
+                                {
+                                    document = reader.ReadToEnd();
+                                    if (document.ToLower().Contains("passwd"))
                                     {
-                                        success = false;
+                                        if (!document.ToLower().Contains("deleted"))
+                                        {
+                                            document = document.Replace(document.Substring((document.IndexOf("passwd") + 7),
+                                                (document.IndexOf("&ps") - (document.IndexOf("passwd") + 7))),
+                                                "DELETED");
+                                            stream.SetLength(text.Length);
+
+                                            using (StreamWriter writer = new StreamWriter(stream))
+                                            {
+                                                writer.Write(document);
+                                                success = true;
+                                            }
+                                        }
+                                        else
+                                        {
+                                            success = false;
+                                        }
                                     }
                                 }
                             }
                         }
-                    }         
+                    }
                 }
+                else
+                {
+                    string harFile = File.ReadAllText(zipPath);
+                    int passwdLocation = harFile.IndexOf("passwd");
+                    string extractedText = harFile.Substring(passwdLocation + 52);
+                    string oldPassword = extractedText.Substring(0, extractedText.IndexOf('"'));
+                    if (oldPassword != null)
+                    {
+                        if (oldPassword.Length != 0 &&
+                            oldPassword != "DELETED")
+                        {
+                            
+                            string newharFile = harFile.Replace(oldPassword, "DELETED");
+
+                            using (StreamWriter writer = new StreamWriter(zipPath))
+                            {
+                                writer.Write(newharFile);
+                                success = true;
+                            }
+                        }
+                        else
+                        {
+                            success = false;
+                        }
+                    }
+                    else
+                    {
+                        success = false;
+                    }
+                }                
                 if (success)
                 {
-                    MessageBox.Show("We could able to find password in the Fiddler trace and successfully deleted the password. Fiddler trace is now safe to be shared.", "Successfully Removed Password", 
+                    MessageBox.Show("Found clear text password and deleted successfuly.", "Successfully Removed Password", 
                         MessageBoxButtons.OK, MessageBoxIcon.Information);
                     progBar.Visible = false;
                 }
                 else
                 {
-                    MessageBox.Show("There is no password found in the Fiddler trace. Fiddler trace is safe to be shared.", "No password found", 
+                    MessageBox.Show("There is no clear text password found in the network trace.", "No password found", 
                         MessageBoxButtons.OK, MessageBoxIcon.Information);
                     progBar.Visible = false;
                 }
@@ -103,13 +141,14 @@ namespace RemoveFiddlerPassword
         {           
             if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                if (Path.GetExtension(openFileDialog1.FileName) == ".saz")
+                if (Path.GetExtension(openFileDialog1.FileName) == ".saz"
+                    || Path.GetExtension(openFileDialog1.FileName) == ".har")
                 {                    
-                    ReadFile(openFileDialog1.FileName);
+                    ReadFile(openFileDialog1.FileName, Path.GetExtension(openFileDialog1.FileName));
                 }
                 else
                 {
-                    MessageBox.Show("This is not a valid Fiddler trace.", "Invalid File...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show("This is not a valid network trace file. Please select a .saz or .har file.", "Invalid File Type...", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
         }        
